4c5743c61178b9b2b7190d04ad6ad50a8039edb3
蒋倩倩 <admin@jqq.local>
2016-11-03 13:58:35 +0800

    【add】工程

---
+++ hello/hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by admin on 2016/11/3.
+//  Copyright © 2016年 jiangqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by admin on 2016/11/3.
+//  Copyright © 2016年 jiangqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
